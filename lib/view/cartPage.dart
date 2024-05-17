897520a7d1ac81e143da0a0c2e2fbb7f6af359ba
Asif-Faizal <moh.asif@protonmail.ch>
2024-05-17 14:00:37 +0530

    hive kore error

--- lib/view/cartPage.dart
+++ lib/view/cartPage.dart
@@ -1,15 +1,124 @@
+import 'dart:async';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:machn_tst/models/product.dart';
 import 'package:machn_tst/view/customerPage.dart';
 import 'package:machn_tst/view/widgets/cartCard.dart';
 
-class CartPage extends StatelessWidget {
-  const CartPage({super.key});
+class CartPage extends StatefulWidget {
+  const CartPage({Key? key}) : super(key: key);
+
+  @override
+  State<CartPage> createState() => _CartPageState();
+}
+
+class _CartPageState extends State<CartPage> {
+  late List<Product> products = [];
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCartProducts();
+  }
+
+  Future<void> _loadCartProducts() async {
+    try {
+      var box = await Hive.openBox<Product>('cart');
+      products = box.values.toList();
+      setState(() {}); // Update the UI after fetching products
+    } catch (e) {
+      print("Error loading cart products: $e");
+    }
+  }
+
+  double calculateSubTotal() {
+    double subTotal = 0;
+    for (var product in products) {
+      subTotal += product.price;
+    }
+    return subTotal;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.all(10),
+        child: BottomAppBar(
+          color: Colors.transparent,
+          height: 100,
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20),
+            child: BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
+              child: Container(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Color.fromARGB(156, 131, 204, 48),
+                ),
+                height: 100,
+                width: 100,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Sub Total',
+                          style: TextStyle(
+                            color: Theme.of(context).colorScheme.secondary,
+                            fontSize: 16,
+                          ),
+                        ),
+                        Text(
+                          '\$${calculateSubTotal().toStringAsFixed(2)}',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).colorScheme.secondary,
+                            fontSize: 26,
+                          ),
+                        ),
+                      ],
+                    ),
+                    ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(18),
+                        ),
+                        backgroundColor: const Color.fromARGB(115, 94, 163, 14),
+                      ),
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => CustomerPage(),
+                          ),
+                        );
+                      },
+                      child: const Padding(
+                        padding: EdgeInsets.symmetric(
+                          horizontal: 30,
+                          vertical: 15,
+                        ),
+                        child: Text(
+                          'CHECKOUT NOW',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
       backgroundColor: Theme.of(context).colorScheme.background,
       appBar: AppBar(
         foregroundColor: Theme.of(context).colorScheme.secondary,
@@ -31,9 +140,10 @@ class CartPage extends StatelessWidget {
                   Navigator.pushNamed(context, '/wishlist');
                 },
                 style: ElevatedButton.styleFrom(
-                    backgroundColor: Colors.white,
-                    shape: const CircleBorder(),
-                    padding: const EdgeInsets.all(10)),
+                  backgroundColor: Colors.white,
+                  shape: const CircleBorder(),
+                  padding: const EdgeInsets.all(10),
+                ),
                 child: Icon(
                   Icons.favorite,
                   size: 28,
@@ -41,102 +151,45 @@ class CartPage extends StatelessWidget {
                 ),
               ),
               Positioned(
-                  top: 5,
-                  right: 5,
-                  child: Container(
-                    height: 15,
-                    width: 15,
-                    decoration: BoxDecoration(
-                        color: Theme.of(context).colorScheme.error,
-                        borderRadius: BorderRadius.circular(10)),
-                    child: const Center(
-                      child: Text(
-                        '0',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 12,
-                            fontWeight: FontWeight.bold),
+                top: 5,
+                right: 5,
+                child: Container(
+                  height: 15,
+                  width: 15,
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).colorScheme.error,
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                  child: const Center(
+                    child: Text(
+                      '0', // Placeholder for number of wishlist items
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 12,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  )),
+                  ),
+                ),
+              ),
             ],
           ),
         ],
       ),
-      body: Stack(
-        children: [
-          ListView.builder(
-              physics: const BouncingScrollPhysics(),
-              padding: const EdgeInsets.all(20),
-              itemCount: 15,
-              itemBuilder: (context, index) {
-                return const CartCard();
-              }),
-          Positioned(
-            bottom: 10,
-            left: 10,
-            right: 10,
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(20),
-              child: BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 5, sigmaY: 5),
-                child: Container(
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(20),
-                    color: const Color.fromARGB(79, 98, 142, 47),
-                  ),
-                  height: 100,
-                  width: 100,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceAround,
-                    children: [
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Text(
-                            'Sub Total',
-                            style: TextStyle(
-                                color: Theme.of(context).colorScheme.secondary,
-                                fontSize: 16),
-                          ),
-                          Text(
-                            '\$65',
-                            style: TextStyle(
-                                fontWeight: FontWeight.bold,
-                                color: Theme.of(context).colorScheme.secondary,
-                                fontSize: 26),
-                          ),
-                        ],
-                      ),
-                      ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(18)),
-                            backgroundColor: Color.fromARGB(115, 70, 117, 17),
-                          ),
-                          onPressed: () {
-                            Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                    builder: (context) => CustomerPage()));
-                          },
-                          child: const Padding(
-                            padding: EdgeInsets.symmetric(
-                                horizontal: 30, vertical: 15),
-                            child: Text(
-                              'CHECKOUT NOW',
-                              style: TextStyle(
-                                  fontSize: 16, fontWeight: FontWeight.bold),
-                            ),
-                          ))
-                    ],
-                  ),
+      body: products.isEmpty
+          ? Center(child: Text('Your cart is empty!'))
+          : Stack(
+              children: [
+                ListView.builder(
+                  physics: const BouncingScrollPhysics(),
+                  padding: const EdgeInsets.all(15),
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    return CartCard(product: products[index]);
+                  },
                 ),
-              ),
+              ],
             ),
-          )
-        ],
-      ),
     );
   }
 }
--- lib/view/widgets/cartCard.dart
+++ lib/view/widgets/cartCard.dart
@@ -1,148 +1,210 @@
 import 'package:flutter/material.dart';
+import 'package:hive/hive.dart';
+import 'package:machn_tst/models/product.dart';
+import 'package:machn_tst/view/ddetailsPage.dart';
 
-class CartCard extends StatelessWidget {
+class CartCard extends StatefulWidget {
+  final Product product;
   const CartCard({
-    super.key,
-  });
+    Key? key,
+    required this.product,
+  }) : super(key: key);
+
+  @override
+  State<CartCard> createState() => _CartCardState();
+}
+
+class _CartCardState extends State<CartCard> {
+  int _counter = 1;
+
+  void _incrementCounter() {
+    setState(() {
+      if (_counter < 9) {
+        _counter++;
+      }
+    });
+  }
+
+  void _decrementCounter() {
+    setState(() {
+      if (_counter > 1) {
+        _counter--;
+      }
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        SizedBox(
-          height: 100,
-          child: Column(
-            children: [
-              const SizedBox(
-                height: 20,
-              ),
-              Stack(
-                children: [
-                  Card(
-                    shape: RoundedRectangleBorder(
-                        borderRadius: BorderRadius.circular(20)),
-                    child: Container(
-                      padding: const EdgeInsets.only(left: 10, right: 20),
-                      height: 70,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        children: [
-                          const SizedBox(
-                            width: 60,
-                            height: 60,
-                          ),
-                          Column(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              Text(
-                                'name',
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    color: Theme.of(context)
-                                        .colorScheme
-                                        .secondary),
-                              ),
-                              const SizedBox(
-                                height: 5,
-                              ),
-                              Row(
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(
+            context,
+            MaterialPageRoute(
+                builder: (context) => DetailsPage(product: widget.product)));
+      },
+      child: Stack(
+        children: [
+          SizedBox(
+            height: 100,
+            child: Column(
+              children: [
+                const SizedBox(
+                  height: 20,
+                ),
+                Stack(
+                  children: [
+                    Card(
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(20)),
+                      child: Container(
+                        padding: const EdgeInsets.only(left: 10, right: 15),
+                        height: 70,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          children: [
+                            const SizedBox(
+                              width: 80,
+                              height: 60,
+                            ),
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
                                   Text(
-                                    '10',
+                                    widget.product.name,
                                     style: TextStyle(
-                                        fontSize: 20,
+                                        overflow: TextOverflow.clip,
                                         fontWeight: FontWeight.bold,
                                         color: Theme.of(context)
                                             .colorScheme
                                             .secondary),
                                   ),
-                                  Text(
-                                    '/kg',
-                                    style: TextStyle(
-                                        fontWeight: FontWeight.bold,
+                                  const SizedBox(
+                                    height: 5,
+                                  ),
+                                  Row(
+                                    children: [
+                                      Text(
+                                        widget.product.price.toStringAsFixed(0),
+                                        style: TextStyle(
+                                            fontSize: 20,
+                                            fontWeight: FontWeight.bold,
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .secondary),
+                                      ),
+                                      Text(
+                                        '/kg',
+                                        style: TextStyle(
+                                            fontWeight: FontWeight.bold,
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .onSurface),
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              ),
+                            ),
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () {
+                                    _decrementCounter();
+                                  },
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.white,
+                                    shape: const CircleBorder(),
+                                    side: BorderSide(
+                                        width: 1.5,
                                         color: Theme.of(context)
                                             .colorScheme
-                                            .onSurface),
+                                            .secondary),
                                   ),
-                                ],
-                              )
-                            ],
-                          ),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: [
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.white,
-                                  shape: const CircleBorder(),
-                                  padding: const EdgeInsets.all(10),
-                                  side: BorderSide(
-                                      width: 1.5,
-                                      color: Theme.of(context)
-                                          .colorScheme
-                                          .secondary),
+                                  child: Icon(
+                                    Icons.remove_rounded,
+                                    size: 16,
+                                    color:
+                                        Theme.of(context).colorScheme.secondary,
+                                  ),
                                 ),
-                                child: Icon(
-                                  Icons.remove_rounded,
-                                  size: 18,
-                                  color:
-                                      Theme.of(context).colorScheme.secondary,
-                                ),
-                              ),
-                              Text('1'),
-                              ElevatedButton(
-                                onPressed: () {},
-                                style: ElevatedButton.styleFrom(
+                                SizedBox(
+                                    width: 3, child: Text(_counter.toString())),
+                                ElevatedButton(
+                                  onPressed: () {
+                                    _incrementCounter();
+                                  },
+                                  style: ElevatedButton.styleFrom(
                                     backgroundColor:
                                         Theme.of(context).colorScheme.secondary,
                                     shape: const CircleBorder(),
-                                    padding: const EdgeInsets.all(10)),
-                                child: const Icon(
-                                  Icons.add_rounded,
-                                  size: 18,
-                                  color: Colors.white,
+                                  ),
+                                  child: const Icon(
+                                    Icons.add_rounded,
+                                    size: 16,
+                                    color: Colors.white,
+                                  ),
                                 ),
+                              ],
+                            ),
+                            SizedBox(
+                              width: 50,
+                              child: Text(
+                                '\$${(widget.product.price * _counter).toStringAsFixed(0)}',
+                                style: TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                    color: Theme.of(context)
+                                        .colorScheme
+                                        .secondary),
                               ),
-                            ],
-                          ),
-                          Text(
-                            '\$6',
-                            style: TextStyle(
-                                fontSize: 24,
-                                fontWeight: FontWeight.bold,
-                                color: Theme.of(context).colorScheme.secondary),
-                          )
-                        ],
+                            )
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  Positioned(
+                    Positioned(
                       top: -6,
                       right: -6,
                       child: IconButton(
-                          onPressed: () {},
-                          icon: Icon(
-                            Icons.clear,
-                            color: Theme.of(context).colorScheme.secondary,
-                            size: 14,
-                          )))
-                ],
+                        onPressed: () {
+                          _delete(widget.product);
+                        },
+                        icon: Icon(
+                          Icons.clear,
+                          color: Theme.of(context).colorScheme.secondary,
+                          size: 14,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Positioned(
+            left: 20,
+            top: 10,
+            child: SizedBox(
+              height: 60,
+              width: 60,
+              child: Image.network(
+                'http://143.198.61.94:8000${widget.product.imageUrl}',
+                fit: BoxFit.cover,
               ),
-            ],
+            ),
           ),
-        ),
-        Positioned(
-          left: 20,
-          top: 10,
-          child: SizedBox(
-            height: 60,
-            width: 60,
-            child: Image.network(
-                'https://atlas-content-cdn.pixelsquid.com/stock-images/small-bottle-of-milk-rv2NEl2-600.jpg'),
-          ),
-        ),
-      ],
+        ],
+      ),
     );
+  }
+
+  void _delete(Product product) async {
+    print(
+        '========================================Deleted ${product.name} from cart==============================================');
+    var box = await Hive.openBox<Product>('cart');
+    box.deleteAt(box.values.toList().indexOf(product));
   }
 }
